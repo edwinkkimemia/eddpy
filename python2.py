3a47e20605c8c887e42d46b4ba8f936d6d9b1bdd
edd <edwinkkimemia@gmail.com>
2025-04-02 09:35:46 +0300

    m

--- python2.py
+++ python2.py
@@ -2,10 +2,15 @@
 # b = int(input("Enter another number: "))
 # operation = input("Enter an operation (+, -, *, /): ")
 
-# languages = [ "Python", "Java", "C++", "JavaScript", "Ruby", "PHP", "Swift", "Go", "Kotlin", "Rust" ]
-# for language in languages:
-#     print(language)
+languages = [ "Python", "Java", "C++", "JavaScript", "Ruby", "PHP", "Swift", "Go", "Kotlin", "Rust" ]
+for language in languages:
+    print(language)
+    if language == "Java":
+        print("Found Java!")
+        break
+# else: 
     
     
 variable = ("hello",)
 print(type(variable))
+
